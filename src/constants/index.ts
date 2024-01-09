94d2d81d7c124cd2eca9261455cc6835e359354c
Rasik Nepal <98022550+rasik-nep@users.noreply.github.com>
2024-01-09 18:17:23 +0545

    feat/footer complete

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,3 +6,36 @@ export const NAV_LINKS = [
   { href: '/pricing', key: 'pricing ', label: 'Pricing ' },
   { href: '/contact_us', key: 'contact_us', label: 'Contact Us' },
 ];
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: 'Learn More',
+    links: [
+      'About Hilink',
+      'Privacy Policy',
+      'Contact Us',
+    ],
+  },
+  {
+    title: 'Our Community',
+    links: ['Climbing xixixi', 'Hiking hilink', 'Hilink kinthill'],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: 'Contact Us',
+  links: [
+    { label: 'Admin Officer', value: '123-456-7890' },
+    { label: 'Email Officer', value: 'hilink@akinthil.com' },
+  ],
+};
+
+export const SOCIALS = {
+  title: 'Social',
+  links: [
+    '/facebook.svg',
+    '/instagram.svg',
+    '/twitter.svg',
+  ],
+};
