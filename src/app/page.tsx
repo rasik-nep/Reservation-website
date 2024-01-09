55012daa1d28ecaf0aaa60c0367d0d8ffb35c147
Rasik Nepal <98022550+rasik-nep@users.noreply.github.com>
2024-01-09 18:36:41 +0545

    feat: added pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
 export default function Home() {
-  return (<h1 className='font-bold text-red-500 bg-blue-200 h-[100px]'>Helaalo three</h1>
+  return (<h1 className='font-bold text-red-500 bg-blue-200 h-[200px]'>Helaalo three</h1>
   )
 }
