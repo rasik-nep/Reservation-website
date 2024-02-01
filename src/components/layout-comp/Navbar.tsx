17aa4b52358303147e446152212bd4b90e811889
Rasik Nepal <98022550+rasik-nep@users.noreply.github.com>
2024-02-01 12:51:48 +0545

    fix: navbar styling

--- src/components/layout-comp/Navbar.tsx
+++ src/components/layout-comp/Navbar.tsx
@@ -32,20 +32,20 @@ const Navbar = () => {
 
     return (
         <nav className="absolute w-full z-10 py-[1.5rem]">
-            <div className="flexBetween flex-wrap max-container">
+            <div className="flexBetween flex-wrap max-container md:px-5">
                 <Link href='/'>
-                    <img src='/logo.png' alt="logo" style={{ width: '100px', height: '70px' }} />
+                    <img src='/logo.png' alt="logo" className="w-[70px] h-[50px] md:w-[100px] md:h-[70px]" />
                 </Link>
                 <ul className="hidden h-full gap-12 lg:flex">
                     <p className="text-sm text-grey-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold hidden lg:inline-block">+1 234 567 890</p>
                     <NavLinks />
                 </ul>
-                <div className="flex flex-row gap-5">
+                <div className="flex flex-row gap-3 md:gap-5">
                     <div className="lg:flexCenter">
                         <Button type="button" title="Book now" variant="btn_transparent" />
                     </div>
                     <p className="text-sm text-grey-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold inline-block lg:hidden">+1 234 567 890</p>
-                    <MdOutlineMenu style={{ width: '32px', height: "32px" }} className=" cursor-pointer lg:hidden" onClick={handleShowMenu} />
+                    <MdOutlineMenu className="w-[32px] h-[32px] cursor-pointer lg:hidden" onClick={handleShowMenu} />
                 </div>
                 {showMenu && <ul className="flex flex-col items-center basis-full">
                     <NavLinks />
